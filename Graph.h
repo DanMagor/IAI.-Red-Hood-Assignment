6e0e55ae1c8d6e3a268a1131d0fe589fe56a3434
Anton Skudarnov <Anton Skudarnov>
2017-09-08 12:00:06 +0300

    graph refactored

--- Graph.h
+++ Graph.h
@@ -12,68 +12,55 @@
 
 using namespace std;
 
-    template<typename V, typename E = double>
-    struct vertex{
-        typedef pair<E, vertex*> edge;
-        vector<edge> edges;
-        V name;
-        vertex(V s) : name(s) {}
-    };
+
 
     template<typename V, typename E=double>
     class Graph {
+
+
+        typedef struct vertex {
+            typedef pair<E, vertex *> edge;
+            vector<edge> edges;
+            V name;
+
+            vertex(V s) : name(s) {}
+        } vertex;
+
     private:
-        typedef map<V,vertex<V,E>*> vmap;
+        typedef map<V, vertex *> vmap;
         vmap vertices;
     public:
-        vector<vertex<V,E>*> GetNeighbors(vertex<V,E>);
+        void AddVertex(V v) {
+            typename vmap::iterator itr = vertices.find(v);
 
-        vector<vertex<V,E>*> GetNeighbors(V);
+            if (itr == vertices.end()) {
+                vertex *temp;
+                temp = new vertex(v);
+                vertices[v] = temp;
+            }
+        }
 
-        void AddVertex(V);
+        void AddEdge(const V from, const V to, E weight=0.0) {
+            vertex *f = (vertices.find(from)->second);
+            vertex *t = (vertices.find(to)->second);
+            pair<E, vertex *> edge = make_pair(weight, t);
+            f->edges.push_back(edge);
+        }
 
-        void AddEdge(const V , const V, E = 0.0);
+        vmap GetVertices() {
+            return vertices;
+        }
 
-        vmap GetVertices();
+        vector<vertex *> GetNeighbors(V v) {
+            vector<vertex *> neighbors;
+            for (auto t:vertices.find(v)->second->edges)
+                neighbors.push_back(t.second);
+            return neighbors;
+        }
 
-        vertex<V,E>* &GetVertex(V);
+
+        vertex *&GetVertex(V);
 
     };
-
-
-template<typename V, typename E>
-void Graph<V,E>::AddVertex(V v) {
-    typename vmap::iterator itr = vertices.find(v);
-
-    if (itr == vertices.end()) {
-        vertex<V> *temp;
-        temp = new vertex<V,E>(v);
-        vertices[v] = temp;
-    }
-}
-
-template<typename V, typename E>
-void Graph<V,E>::AddEdge(const V from, const V to, E weight) {
-    vertex<V,E> *f = (vertices.find(from)->second);
-    vertex<V,E> *t = (vertices.find(to)->second);
-    pair<E, vertex<V,E>*> edge = make_pair(weight,t);
-    f->edges.push_back(edge);
-}
-
-template<typename V, typename E>
-map<V,vertex<V,E>*> Graph<V,E>::GetVertices() {
-    return vertices;
-}
-
-
-template<typename V, typename E>
-vector<vertex<V, E>*> Graph<V,E>::GetNeighbors(V v) {
-    vector<vertex<V,E>*> neighbors;
-    for(auto t:vertices.find(v)->second->edges)
-        neighbors.push_back(t.second);
-    return neighbors;
-}
-
-
 
 #endif //ASSIGNMENT1_GRAPH_H
